1ccd93ebf9c68efccae6bac0a7f66853f2a1473f
cuddleminddev <cuddleminddev@gmail.com>
2025-09-11 11:22:36 +0530

    projects

--- src/app/components/project-detail/project-detail.component.ts
+++ src/app/components/project-detail/project-detail.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Projects } from '../../data/projects';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-detail',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.css'
 })
-export class ProjectDetailComponent {
+export class ProjectDetailComponent implements OnInit{
+  project: any;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.project = Projects.find(p => p.id === id);
+  }
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "../../shared/banner/banner.component";
+import { CommonModule } from '@angular/common';
+import { Projects } from '../../data/projects';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [BannerComponent, CommonModule, RouterModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects = Projects
 }
