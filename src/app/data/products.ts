358e1538743417edc12579261952ad220aeb910c
cuddleminddev <cuddleminddev@gmail.com>
2025-09-11 14:14:19 +0530

    images upload

---
+++ src/app/data/products.ts
@@ -0,0 +1,15 @@
+export const Products = [
+
+    {
+        id: 1,
+        name: 'VRV IV +',
+        short: 'Our new VRV IV heat recovery systems set pioneering  standards in all-round climate comfort performance.  Total design simplicity, offering rapid installation, full  f lexibility as well as absolute efficiency and comfort',
+        imgPoints: [
+            {
+                no: 1,
+                head: ''
+            }
+        ]
+    }
+    
+]
