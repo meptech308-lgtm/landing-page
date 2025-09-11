e0ac3715e617fcf5fd3701a7a83b8c3028375048
cuddleminddev <cuddleminddev@gmail.com>
2025-09-11 09:15:49 +0530

    contact basic

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "../../shared/banner/banner.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [BannerComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fullName: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      enquiry: ['', Validators.required],
+      details: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      alert('Form submitted successfully!');
+      this.contactForm.reset();
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
