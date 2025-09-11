6bbdda274140803bc6af5507ece7fd8898f81f75
cuddleminddev <cuddleminddev@gmail.com>
2025-09-11 15:33:42 +0530

    products list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
@@ -10,11 +8,10 @@ import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'products', component: ProductsComponent },
-    { path: 'products/:id', component: ProductDetailComponent },
     { path: 'services', component: ServicesComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'projects/:id', component: ProjectDetailComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'products', loadChildren: () => import('./details/details.module').then(m => m.DetailsModule) }
 ];
--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -1,15 +1,112 @@
 export const Products = [
-
     {
         id: 1,
-        name: 'VRV IV +',
-        short: 'Our new VRV IV heat recovery systems set pioneering  standards in all-round climate comfort performance.  Total design simplicity, offering rapid installation, full  f lexibility as well as absolute efficiency and comfort',
-        imgPoints: [
-            {
-                no: 1,
-                head: ''
-            }
-        ]
+        img: '/products/vrviv+/main.png',
+        name: 'VRV IV +'
+    },
+    {
+        id: 2,
+        img: '/products/vrvx/main.png',
+        name: 'VRV X'
+    },
+    {
+        id: 3,
+        img: '/products/vrvapt/main.png',
+        name: 'VRV APT'
+    },
+    {
+        id: 4,
+        img: '/products/concealedac/main.png',
+        name: 'Concealed AC Unit'
+    },
+    {
+        id: 5,
+        img: '/products/wallsplit/main.png',
+        name: 'Wall Split'
+    },
+    {
+        id: 6,
+        img: '/products/rooftoppackage/main.png',
+        name: 'Roof top Package'
+    },
+    {
+        id: 7,
+        img: '/products/cassette/main.png',
+        name:'Cassette Solutions'
+    },
+    {
+        id: 8,
+        img: '/products/airhandling/main.png',
+        name: 'Air Handling Units'
+    },
+    {
+        id: 9,
+        img: '/products/fancoil/main.png',
+        name: 'Fan coil units'
+    },
+    {
+        id: 10,
+        img: '/products/soundatte/main.png',
+        name: 'Sound Attenuators'
+    },
+    {
+        id: 11,
+        img: '/products/motorized/main.png',
+        name: 'Motorized Smoke Fire Damper'
+    },
+    {
+        id: 12,
+        img: '/products/lineargrills/main.png',
+        name: 'Linear Grills'
+    },
+    {
+        id: 13,
+        img: '/products/linearslot/main.png',
+        name: 'Linear Slot Diffuser'
+    },
+    {
+        id: 14,
+        img: '/products/volumecontrol/main.png',
+        name: 'Volume Control Damper'
+    },
+    {
+        id: 15,
+        img: '/products/firedamper/main.png',
+        name: 'Fire Damper'
+    },
+    {
+        id: 16,
+        img: '/products/controlunits/main.png',
+        name: 'Control Units'
+    },
+    {
+        id: 17,
+        img: '/products/ceilingdiffu/main.png',
+        name: 'Ceiling diffusers'
+    },
+    {
+        id: 18,
+        img: '/products/variablereturn/main.png',
+        name: 'Variable Return Damper'
+    },
+    {
+        id: 19,
+        img: '/products/nonreturndam/main.png',
+        name: 'Non Return Dampers'
+    },
+    {
+        id: 20,
+        img: '/products/ecologyunit/main.png',
+        name: 'Ecology Unit'
+    },
+    {
+        id: 21,
+        img: '/products/thermostat/main.png',
+        name: 'Thermostat'
+    },
+    {
+        id: 22,
+        img: '/products/thermostat2/main.png',
+        name: 'Thermostat'
     }
-    
 ]
