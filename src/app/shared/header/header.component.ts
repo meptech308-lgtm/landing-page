9643aaf6fc101ec7663a4a8d88b1e57235fa092d
cuddleminddev <cuddleminddev@gmail.com>
2025-09-15 13:51:56 +0530

    drop down

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Products } from '../../data/products';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +11,39 @@ import { RouterModule } from '@angular/router';
 })
 export class HeaderComponent {
   menuOpen = false;
+  dropdownOpen = false;
+  categories: any[] = [];
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  constructor() {
+    const grouped: { [key: string]: any[] } = {};
+    Products.forEach(p => {
+      if (!grouped[p.category]) grouped[p.category] = [];
+      grouped[p.category].push(p);
+    });
+
+    this.categories = Object.keys(grouped).map(key => ({
+      name: key,
+      items: grouped[key],
+      open: false 
+    }));
+  }
+
+  toggleDropdown(event: Event) {
+    event.preventDefault();
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  openCategory(cat: any) {
+    if (window.innerWidth >= 992) {
+      cat.open = true;
+    }
+  }
+
+  toggleCategory(event: Event, cat: any) {
+    event.preventDefault();
+    if (window.innerWidth < 992) {
+      cat.open = !cat.open;
+    }
   }
 
   closeNavbar() {
