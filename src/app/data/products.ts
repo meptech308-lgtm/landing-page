8f29c14d38f09a2eaf1df7a8de0fe7f725826e38
cuddleminddev <cuddleminddev@gmail.com>
2025-09-15 13:35:09 +0530

    array change

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -3,132 +3,154 @@ export const Products = [
         id: 1,
         img: '/products/vrviv+/main.png',
         name: 'VRV IV +',
-        route: 'vrviv'
+        route: 'vrviv',
+        category: 'AC Equipments'
     },
     {
         id: 2,
         img: '/products/vrvx/main.png',
         name: 'VRV X',
-        route: 'vrvx'
+        route: 'vrvx',
+        category: 'AC Equipments'
     },
     {
         id: 3,
         img: '/products/vrvapt/main.png',
         name: 'VRV APT',
-        route: 'vrvapt'
+        route: 'vrvapt',
+        category: 'AC Equipments'
     },
     {
         id: 4,
         img: '/products/concealedac/main.jpg',
         name: 'Concealed AC Unit',
-        route: 'concealedac'
+        route: 'concealedac',
+        category: 'AC Equipments'
     },
     {
         id: 5,
         img: '/products/wallsplit/main.png',
         name: 'Wall Split',
-        route: 'wallsplit'
+        route: 'wallsplit',
+        category: 'AC Equipments'
     },
     {
         id: 6,
         img: '/products/rooftoppackage/main.png',
         name: 'Roof top Package',
-        route: 'rooftoppackage'
+        route: 'rooftoppackage',
+        category: 'AC Equipments'
     },
     {
         id: 7,
         img: '/products/cassette/main.png',
         name: 'Cassette Solutions',
-        route: 'cassette'
+        route: 'cassette',
+        category: 'AC Equipments'
     },
     {
         id: 8,
         img: '/products/airhandling/main.png',
         name: 'Air Handling Units',
-        route: 'airhandling'
+        route: 'airhandling',
+        category: 'Air Distribution'
     },
     {
         id: 9,
         img: '/products/fancoil/main.png',
         name: 'Fan coil units',
-        route: 'fancoilunits'
+        route: 'fancoilunits',
+        category: 'Air Distribution'
     },
     {
         id: 10,
         img: '/products/soundatte/main.jpg',
         name: 'Sound Attenuators',
-        route: 'soundatte'
+        route: 'soundatte',
+        category: 'Ventilation'
     },
     {
         id: 11,
         img: '/products/motorized/main.jpg',
         name: 'Motorized Smoke Fire Damper',
-        route: 'motorized'
+        route: 'motorized',
+        category: 'Ventilation'
     },
     {
         id: 12,
         img: '/products/lineargrills/main.png',
         name: 'Linear Grills',
-        route: 'lineargrills'
+        route: 'lineargrills',
+        category: 'Air Distribution'
     },
     {
         id: 13,
         img: '/products/linearslot/main.png',
         name: 'Linear Slot Diffuser',
-        route: 'linerslotdiffuser'
+        route: 'linerslotdiffuser',
+        category: 'Air Distribution'
     },
     {
         id: 14,
         img: '/products/volumecontrol/main.png',
         name: 'Volume Control Damper',
-        route: 'volumecontrol'
+        route: 'volumecontrol',
+        category: 'Controls'
     },
     {
         id: 15,
         img: '/products/firedamper/main.png',
         name: 'Fire Damper',
-        route: 'firedamper'
+        route: 'firedamper',
+        category: 'Ventilation'
     },
     {
         id: 16,
         img: '/products/controlunits/main.png',
         name: 'Control Units',
-        route: 'controlunits'
+        route: 'controlunits',
+        category: 'Controls'
     },
     {
         id: 17,
         img: '/products/ceilingdiffu/main.png',
         name: 'Ceiling diffusers',
-        route: 'ceilingdiffusers'
+        route: 'ceilingdiffusers',
+        category: 'Air Distribution'
     },
     {
         id: 18,
         img: '/products/variablereturn/main.png',
         name: 'Variable Return Damper',
-        route: 'variablereturndamper'
+        route: 'variablereturndamper',
+        category: 'Ventilation'
     },
     {
         id: 19,
         img: '/products/nonreturndam/main.png',
         name: 'Non Return Dampers',
-        route: 'nonreturndampers'
+        route: 'nonreturndampers',
+        category: 'Ventilation'
     },
     {
         id: 20,
         img: '/products/ecologyunit/main.png',
         name: 'Ecology Unit',
-        route: 'ecologyunits'
+        route: 'ecologyunits',
+        category: 'Ventilation'
     },
     {
         id: 21,
         img: '/products/thermostat/main.png',
         name: 'Thermostat',
-        route: 'thermostat'
+        route: 'thermostat',
+        category: 'Controls'
     },
     {
         id: 22,
         img: '/products/thermostat2/main.png',
         name: 'Thermostat',
-        route: 'thermostat2'
+        route: 'thermostat2',
+        category: 'Controls'
     }
 ];
--- src/app/details/list/list.component.ts
+++ src/app/details/list/list.component.ts
@@ -25,36 +25,11 @@ export class ListComponent {
     'Controls'
   ];
 
-  categoryMap: { [key: number]: string } = {
-    1: 'AC Equipments',
-    2: 'AC Equipments',
-    3: 'AC Equipments',
-    4: 'AC Equipments',
-    5: 'AC Equipments',
-    6: 'AC Equipments',
-    7: 'AC Equipments',
-    8: 'Air Distribution',
-    9: 'Air Distribution',
-    10: 'Ventilation',
-    11: 'Ventilation',
-    12: 'Air Distribution',
-    13: 'Air Distribution',
-    14: 'Controls',
-    15: 'Ventilation',
-    16: 'Controls',
-    17: 'Air Distribution',
-    18: 'Ventilation',
-    19: 'Ventilation',
-    20: 'Ventilation',
-    21: 'Controls',
-    22: 'Controls'
-  };
-
   applyFilters() {
     this.filteredProducts = this.products.filter(p => {
       const matchesSearch = p.name.toLowerCase().includes(this.searchText.toLowerCase());
       const matchesCategory = this.selectedCategory
-        ? this.categoryMap[p.id] === this.selectedCategory
+        ? p.category === this.selectedCategory
         : true;
 
       return matchesSearch && matchesCategory;
