9e84c0846c8c33782b3ff49836f9757c5fe0c2b6
mudfayaz44 <muhdfayaz44@gmail.com>
2026-03-21 10:41:21 +0530

    seo crawlable links added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
-import { FooterComponent } from "./shared/footer/footer.component";
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { GtmService } from './service/gtm.service';
 import { filter, map, mergeMap } from 'rxjs';
 import { Title } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 declare let gtag: Function;
 
@@ -12,7 +19,7 @@ declare let gtag: Function;
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'meptech';
@@ -21,36 +28,39 @@ export class AppComponent implements OnInit {
     private router: Router,
     private gtm: GtmService,
     private route: ActivatedRoute,
-    private titleService: Title
-  ) { }
-
+    private titleService: Title,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {}
   ngOnInit(): void {
-    this.router.events.subscribe(event => {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        gtag('config', 'G-2ML3SZ46T9', {
-          page_path: event.urlAfterRedirects
-        });
+        if (isPlatformBrowser(this.platformId)) {
+          if ((window as any).gtag) {
+            (window as any).gtag('config', 'G-XXXX');
+          }
+        }
       }
     });
 
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(() => {
-        let route = this.route;
-        while (route.firstChild) route = route.firstChild;
-        return route;
-      }),
-      mergeMap(route => route.data)
-    ).subscribe(data => {
-      const pageTitle = data['title'] || this.titleService.getTitle();
-      const pagePath = this.router.url;
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.route;
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        mergeMap((route) => route.data),
+      )
+      .subscribe((data) => {
+        const pageTitle = data['title'] || this.titleService.getTitle();
+        const pagePath = this.router.url;
 
-      this.gtm.pushEvent({
-        event: 'page_view',
-        page_title: pageTitle,
-        page_path: pagePath,
+        this.gtm.pushEvent({
+          event: 'page_view',
+          page_title: pageTitle,
+          page_path: pagePath,
+        });
       });
-    });
   }
-
 }
---
+++ src/app/app.config.server.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
+import { provideServerRouting } from '@angular/ssr';
+import { appConfig } from './app.config';
+import { serverRoutes } from './app.routes.server';
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRouting(serverRoutes),
+  ],
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
