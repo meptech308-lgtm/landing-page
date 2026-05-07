ef0e07388f56da230ebc13033ddee2e970c977bb
mudfayaz44 <muhdfayaz44@gmail.com>
2026-05-07 14:24:23 +0530

    contact bug fix

--- src/app/components/seopages/daikin-fcu-suppliers-in-dubai/daikin-fcu-suppliers-in-dubai.component.spec.ts
+++ src/app/components/seopages/daikin-fcu-suppliers-in-dubai/daikin-fcu-suppliers-in-dubai.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DaikinFcuSuppliersInDubaiComponent } from './daikin-fcu-suppliers-in-dubai.component';
+import { DaikinFcuSuppliersDubaiComponent } from './daikin-fcu-suppliers-in-dubai.component';
 
-describe('DaikinFcuSuppliersInDubaiComponent', () => {
-  let component: DaikinFcuSuppliersInDubaiComponent;
-  let fixture: ComponentFixture<DaikinFcuSuppliersInDubaiComponent>;
+describe('DaikinFcuSuppliersDubaiComponent', () => {
+  let component: DaikinFcuSuppliersDubaiComponent;
+  let fixture: ComponentFixture<DaikinFcuSuppliersDubaiComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DaikinFcuSuppliersInDubaiComponent]
-    })
-    .compileComponents();
+      imports: [DaikinFcuSuppliersDubaiComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(DaikinFcuSuppliersInDubaiComponent);
+    fixture = TestBed.createComponent(DaikinFcuSuppliersDubaiComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
